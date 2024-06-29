0fb3a9dd2af20c1d578f15dfcfc81f3f552fa98e
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-06-29 04:04:44 +0300

    final

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,3 +1,9 @@
 # Using Puppet, install flask from pip3
-file { 'flask':
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    }
+package { 'werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3',
     }
