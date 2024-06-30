8d6a965d72ef8b00b0b412d3c240712ec0a9fef3
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-07-01 01:03:24 +0300

    unstod

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,8 @@
 # set up your client SSH configuration file so that you can connect to a server without typing a password
 include stdlib
 file_line { 'ssh_config':
-    ensure  => present
     path    => '/etc/ssh/ssh_config',
-    line    => '^.+PasswordAuthentication no',
-    match   => '^.+PasswordAuthentication yes',
+    line    => '#   PasswordAuthentication no',
+    match   => '.+PasswordAuthentication yes',
     replace => true,
     }
