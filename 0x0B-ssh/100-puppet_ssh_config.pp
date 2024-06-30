56cb46c20fb0356768e2a6351307ba99e7dabd7e
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-07-01 00:34:40 +0300

    tst

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,8 @@
 # set up your client SSH configuration file so that you can connect to a server without typing a password
 file_line { 'ssh_config':
-    ensure => present
-    path   => '/etc/ssh/ssh_config',
-    line   => 'PasswordAuthentication no',
-    match  => 'PasswordAuthentication yes',
+    ensure  => present
+    path    => '/etc/ssh/ssh_config',
+    line    => 'PasswordAuthentication no',
+    match   => 'PasswordAuthentication yes',
+    replace => true,
     }
