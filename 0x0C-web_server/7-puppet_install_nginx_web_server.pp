32e49812f0e66165947b3b7f6ead9ec3ea605901
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-07-11 00:27:05 +0300

    n

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -14,11 +14,11 @@ file {'/var/www/html/index.html':
 }
 
 exec {'redirect_me':
-  command  => 'sed -i "/server_name _;/a \\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}" /etc/nginx/sites-available/default',
-  provider => 'shell',
+  command  => 'sed -i "24i\	rewrite ^/redirect_me$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell'
 }
 
 service {'nginx':
   ensure  => running,
-  require => Package['nginx'],
+  require => Package['nginx']
 }
