59a5568c7c07185b1580b970c521209ded81ee59
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-07-11 00:08:20 +0300

    >

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,29 @@
 # a Puppet manifest containing commands to automatically configure an Ubuntu machine to respect above requirements
 exec { 'update':
     command  => '/usr/bin/apt-get update'
+    path     => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
 }
 package { 'nginx':
-    ensure   => installed
-    provider => apt
+    ensure   => installed,
+    provider => apt,
+    require  => Exec['update'],
 }
 file { 'index.html':
     ensure  => 'present',
     path    => '/var/www/html/index.html',
-    content => 'Hello World!'
+    content => 'Hello World!',
+    require => Package['nginx'],
 }
 exec {'redirect_me':
     command  => 'sed -i "/server_name _;/a \\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}" /etc/nginx/sites-available/default',
-    provider => 'shell'
+    path     => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+    provider => 'shell',
+    require  => Package['nginx'],
+    notify   => Service['nginx'],
 }
 service { 'nginx':
-    ensure  => 'running',
-    require => Package['nginx']
+    ensure    => 'running',
+    enable    => true,
+    require   => Package['nginx'],
+    subscribe => Exec['redirect_me'],
 }
