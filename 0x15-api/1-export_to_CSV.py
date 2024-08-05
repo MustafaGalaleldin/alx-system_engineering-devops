40d85cbd047ed8624bf3a6eff675fe2bd7d64119
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-08-05 23:37:10 +0300

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""
+extend your Python script to export data in the CSV format
+"""
+if __name__ == "__main__":
+
+    import requests
+    import csv
+    import sys
+
+    user_id = int(sys.argv[1])
+    todo_response = requests.get("https://jsonplaceholder.typicode.com/todos/")
+    todo_list = todo_response.json()
+    user_response = requests.get("https://jsonplaceholder.typicode.com/users/")
+    user_list = user_response.json()
+    user_name = ""
+    results = []
+    fields = ["userId", "name", "completed", "title"]
+    for user in user_list:
+        if user.get("id") == user_id:
+            user_name = user.get("name")
+            break
+    for todo in todo_list:
+        if todo.get("userId") == user_id:
+            temp_dict = {k: v for k, v in todo.items() if k != "id"}
+            temp_dict["name"] = user_name
+            results.append(temp_dict)
+    with open(f"{user_id}.csv", "w") as csvfile:
+        writer = csv.DictWriter(f=csvfile, fieldnames=fields)
+        writer.writerows(results)
