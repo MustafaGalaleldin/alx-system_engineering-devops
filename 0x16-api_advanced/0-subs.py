146a7b3c2efa36d270843790fe0552f475620ee6
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-08-13 19:42:19 +0300

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"a function that queries the Reddit API and returns the number of subscribers"
+import requests
+
+
+def number_of_subscribers(subreddit):
+    "returns the number of subscribers or 0 if invalid"
+    subscribers = 0
+    url = f"https://api.reddit.com/r/{subreddit}/about"
+    try:
+        response = requests.get(url)
+        result = response.json()
+        for k, v in result["data"].items():
+            if k == "subscribers":
+                subscribers = v
+                break
+    except Exception:
+        pass
+    return subscribers
