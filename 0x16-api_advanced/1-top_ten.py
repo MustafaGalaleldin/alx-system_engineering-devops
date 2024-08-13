66ec73eaa88fefa8b80525bd3abad476af1dba66
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-08-13 23:45:10 +0300

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+a function that queries the Reddit API and  prints the titles
+of the first 10 hot posts listed for a given subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    "print the 10 titles"
+    url = f"https://api.reddit.com/r/{subreddit}/hot"
+    headers = {"User-Agent": "python:subreddit.subscriber.counter:v1.0"}
+    try:
+        count = 0
+        response = requests.get(url, allow_redirects=False, headers=headers)
+        result = response.json()
+        for dic in result["data"]["children"]:
+            if count < 10:
+                print(dic["data"].get("title"))
+                count += 1
+            else:
+                break
+    except Exception:
+        print(None)
