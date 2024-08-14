ef09f3d4f2726ecfb380ea127f0220ce04dedc0a
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-08-14 04:46:43 +0300

    cleaned

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"a function that queries the Reddit API and returns the number of subscribers"
+"a function that queries Reddit API and returns the number of subscribers"
 import requests
 
 
