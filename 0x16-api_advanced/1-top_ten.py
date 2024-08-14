00e8a9f738e976aa38969b58935f6f3a5a92c5ab
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-08-14 04:43:39 +0300

    test

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,17 +9,11 @@ import requests
 def top_ten(subreddit):
     "print the 10 titles"
     url = f"https://api.reddit.com/r/{subreddit}/hot"
-    headers = {"User-Agent": "python:subreddit.subscriber.counter:v1.0"}
     try:
-        count = 0
         response = requests.get(url, allow_redirects=False, headers=headers)
         if response.status_code == 200:
             result = response.json()
             for dic in result["data"]["children"]:
-                if count < 10:
-                    print(dic["data"].get("title"))
-                    count += 1
-                else:
-                    break
+                print(dic["data"].get("title"))
     except Exception:
         print(None)
